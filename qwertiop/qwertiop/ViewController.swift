de4ac7f72bc072f6869bd7b67cb27ca5746f8d60
Veronika Stoilova <veronika.stoilova@VeronikaS.local>
2016-07-19 10:19:07 +0300

    stuff

---
+++ qwertiop/qwertiop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  qwertiop
+//
+//  Created by Veronika Stoilova on 7/19/16.
+//  Copyright © 2016 Veronika Stoilova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
